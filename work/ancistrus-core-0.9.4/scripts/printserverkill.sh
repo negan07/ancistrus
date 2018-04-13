76d1289e85041b428a0e3f079295b55a50934e22
negan07 <negan07@users.noreply.github.com>
2018-04-13 21:06:57 +0200

    core: printserverkill.sh revisited to simplify inhibition webgui page

--- work/ancistrus-core-0.9.4/scripts/printserverkill.sh
+++ work/ancistrus-core-0.9.4/scripts/printserverkill.sh
@@ -1,11 +1,39 @@
 #!/bin/sh
 
+KILL_PS() {
 # kill print server
-eval `nvram get printserver_disable` > /dev/null 2>&1
-if [ "${printserver_disable}" = "1" ]; then
-killall -9 KC_BONJOUR > /dev/null 2>&1
-sleep 1
-killall -9 KC_PRINT > /dev/null 2>&1
-sleep 1
-fi
+for K in "KC_PRINT" "KC_BONJOUR"
+do
+	for PID in `pidof ${K}`
+	do
+	[ -e /proc/${PID}/stat ] && while kill -9 $PID >/dev/null 2>&1; do :; done
+	done
+done
+}
+
+RUN_PS() {
+# run print server
+KC_BONJOUR &
+KC_PRINT &
+/etc/turn_on_printer_led
+}
+
+[ -z "$1" ] && echo "Usage: $0 <disablekill|enablekill|stop>" && exit 1
+
+eval `nvram get printserver_disable` >/dev/null 2>&1
+
+case "$1" in
+disablekill)											#stop, enable, then run printserver
+KILL_PS
+nvram set printserver_disable=0
+RUN_PS >/dev/null 2>&1
+;;
+enablekill)											#disable & stop printserver
+nvram set printserver_disable=1
+KILL_PS
+;;
+stop)												#for runlevel
+[ "${printserver_disable}" = "1" ] && KILL_PS
+;;
+esac
 
