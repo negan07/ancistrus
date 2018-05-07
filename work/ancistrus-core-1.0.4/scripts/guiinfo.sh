f9d757294b3a55bc89ed20a6f02446ad388e9f1e
negan07 <negan07@users.noreply.github.com>
2018-05-07 21:16:55 +0200

    core/guiinfo.sh: added rep mirror ping test before opkg update

--- work/ancistrus-core-1.0.4/scripts/guiinfo.sh
+++ work/ancistrus-core-1.0.4/scripts/guiinfo.sh
@@ -14,16 +14,22 @@
 # Usage: $0 >/dev/null
 #
 
-xdslctl info --version 2>/tmp/ancdslinfo			#xdslctl uses stderr for drv version show
-xdslctl info --vendor >>/tmp/ancdslinfo				#append vendor
-xdslctl info --stats | grep Profile >>/tmp/ancdslinfo		#vdsl profile
-xdslctl info --vectoring | grep Vectoring >>/tmp/ancdslinfo	#vectoring state
-xdslctl info --stats | grep SNR >>/tmp/ancdslinfo		#snr dl/ul
-xdslctl info --stats | grep Attn >>/tmp/ancdslinfo		#line att dl/ul
-xdslctl info --stats | grep Pwr >>/tmp/ancdslinfo		#line pwr dl/ul
-eval `nvram get anc_upgr_disable` >/dev/null 2>&1
-	if [ "$anc_upgr_disable" != "1" ]; then			#update rep
-	opkg update >/dev/null 2>&1
-	opkg list-upgradable >/tmp/opkg/list-upgradable		#opkg not creating a file list-upgradable so redir the output
+DSLINFO=/tmp/ancdslinfo
+LISTUPGR=/tmp/opkg/list-upgradable
+
+xdslctl info --version 2>${DSLINFO}					#xdslctl uses stderr for drv version show
+xdslctl info --vendor >>${DSLINFO}					#append vendor
+xdslctl info --stats | grep Profile >>${DSLINFO}			#vdsl profile
+xdslctl info --vectoring | grep Vectoring >>${DSLINFO}			#vectoring state
+xdslctl info --stats | grep SNR >>${DSLINFO}				#snr dl/ul
+xdslctl info --stats | grep Attn >>${DSLINFO}				#line att dl/ul
+xdslctl info --stats | grep Pwr >>${DSLINFO}				#line pwr dl/ul
+eval `nvram get anc_upgr_disable` >/dev/null 2>&1			#perform rep update
+	if [ "$anc_upgr_disable" != "1" -a -x /usr/sbin/opkg ]; then
+	ping -4 -c 1 -w 1 -W 1 -q `cat /usr/etc/opkg.conf | grep "src/gz " | grep -v "#" | head -n 1 | awk '{printf $3}' | cut -f 3- -d "/" | cut -f 1 -d "/"`							#test if main rep is up
+		if [ $? -eq 0 ]; then					#ping is good update rep
+		opkg update >/dev/null 2>&1
+		opkg list-upgradable >${LISTUPGR}			#opkg not creating a file list-upgradable so redir the output
+		fi
 	fi
 exit 0
