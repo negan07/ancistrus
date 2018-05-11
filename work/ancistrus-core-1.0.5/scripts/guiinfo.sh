205f9d5ee45afac7ca3e50aebd0c6faf5f285050
negan07 <negan07@users.noreply.github.com>
2018-05-11 20:47:05 +0200

    core/guiinfo.sh: small code tuning

--- work/ancistrus-core-1.0.5/scripts/guiinfo.sh
+++ work/ancistrus-core-1.0.5/scripts/guiinfo.sh
@@ -19,14 +19,11 @@ LISTUPGR=/tmp/opkg/list-upgradable
 
 xdslctl info --version 2>${DSLINFO}					#xdslctl uses stderr for drv version show
 xdslctl info --vendor >>${DSLINFO}					#append vendor
-xdslctl info --stats | grep Profile >>${DSLINFO}			#vdsl profile
 xdslctl info --vectoring | grep Vectoring >>${DSLINFO}			#vectoring state
-xdslctl info --stats | grep SNR >>${DSLINFO}				#snr dl/ul
-xdslctl info --stats | grep Attn >>${DSLINFO}				#line att dl/ul
-xdslctl info --stats | grep Pwr >>${DSLINFO}				#line pwr dl/ul
+for G in Profile SNR Attn Pwr; do xdslctl info --stats | grep $G >>${DSLINFO}; done
 eval `nvram get anc_upgr_disable` >/dev/null 2>&1			#perform rep update
 	if [ "$anc_upgr_disable" != "1" -a -x /usr/sbin/opkg ]; then
-	ping -4 -c 1 -w 1 -W 1 -q `cat /usr/etc/opkg.conf | grep "src/gz " | grep -v "#" | head -n 1 | awk '{printf $3}' | cut -f 3- -d "/" | cut -f 1 -d "/"`							#test if main rep is up
+	ping -4 -c 1 -w 1 -W 1 -q `cat /usr/etc/opkg.conf | grep "src/gz " | grep -v "#" | head -n 1 | awk '{printf $3}' | cut -f 3- -d "/" | cut -f 1 -d "/"` >/dev/null 2>&1					#test if main rep is up
 		if [ $? -eq 0 ]; then					#ping is good update rep
 		opkg update >/dev/null 2>&1
 		opkg list-upgradable >${LISTUPGR}			#opkg not creating a file list-upgradable so redir the output
