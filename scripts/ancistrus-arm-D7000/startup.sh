ecb9af93dc849ab6f1297144b93bbc6d3dc961ef
negan07 <negan07@users.noreply.github.com>
2019-03-08 22:04:59 +0100

    opkg bumped to 0.4.0-011

--- scripts/ancistrus-arm-D7000/startup.sh
+++ scripts/ancistrus-arm-D7000/startup.sh
@@ -49,14 +49,14 @@ LANGSDIR=${WWW}/langs
 URL=https://raw.githubusercontent.com/negan07/ancistrus/gh-pages/tools/ancistrus-arm-D7000
 [ ! -z "$1" ] && URL=$1
 
-[ -x ${BINDIR}/${BIN} ] || mount | grep ${MNTPART} ] && echo "${BIN} looks already installed." && exit 4
+[ -x ${BINDIR}/${BIN} ] || mount | grep ${MNTPART} >/dev/null 2>&1 && echo "${BIN} looks already installed." && exit 4
 
 cd ${ETCDIR}
 echo "Cleaning up some garbage/orphan dirs & files..."
 [ ! -z "`pidof telnetenabled`" ] && killall -9 telnetenabled
-for D in "/opt" "${WWW}/cgi-bin"; do find $D -type d -name .svn -exec rm -rf '{}' \; > /dev/null 2>&1; done
-for D in "CSY" "TUR"; do find ${LANGSDIR} -type d -name $D -exec rm -rf '{}' \; > /dev/null 2>&1; done
-find ${LANGSDIR} -type f -name *.gz -exec rm -f '{}' \; > /dev/null 2>&1
+for D in "/opt" "${WWW}/cgi-bin"; do find $D -type d -name .svn -exec rm -rf '{}' \; >/dev/null 2>&1; done
+for D in "CSY" "TUR"; do find ${LANGSDIR} -type d -name $D -exec rm -rf '{}' \; >/dev/null 2>&1; done
+find ${LANGSDIR} -type f -name *.gz -exec rm -f '{}' \; >/dev/null 2>&1
 rm -f ${OPKG} ${CONF} ${ARC} ${USRETCDIR}/rcS.MT ${BINDIR}/reaim ${BINDIR}/telnetenabled ${WWW}/language/Czech.js ${WWW}/*DGND*.jpg ${WWW}/vw_* ${WWW}/vpn_* ${WWW}/h_vpn* ${WWW}/h_vauto* ${WWW}/h_vman* ${WWW}/index1.htm ${WWW}/start1.htm ${WWW}/*_demo.htm ${WWW}/start_old_style.htm ${WWW}/ORG-RST_status.htm ${WWW}/DSL_PRO_config.html
 sync
 echo
@@ -69,8 +69,7 @@ chmod 644 ${CONF}
 echo
 echo "Mounting opkg info & status files partition..."
 [ ! -d ${MNTDIR} ] && mkdir -m 0777 ${MNTDIR}
-umount ${MNTDIR} >/dev/null 2>&1
-umount ${MNTLANGPART} >/dev/null 2>&1
+for U in ${MNTDIR} ${MNTLANGPART}; do umount $U >/dev/null 2>&1; done
 mount -n -t jffs2 ${MNTPART} ${MNTDIR}
 [ $? -ne 0 ] && echo "Problem has occurred: opkg partition not mounted." && exit 2
 rm -rf ${MNTDIR}/*
