148333cbbbb7cd521687a41e3087de550642d16e
negan07 <negan07@users.noreply.github.com>
2021-02-01 22:23:26 +0100

    speedtest-net: updated cert url, added nvram lic flags, added setlic clean funcs, improved code

--- work/speedtest-net-1.0.1/speedtest-net.sh
+++ work/speedtest-net-1.0.1/speedtest-net.sh
@@ -16,7 +16,10 @@
 # 		https://www.speedtest.net/about/terms
 # 		https://www.speedtest.net/about/privacy
 #
-# Usage: $0 (<...> ... <...>)
+# Usage: $0 (<setlics>|<clean>) (<...> ... <...>)
+#
+# setlics = setup license flag storing them on nvram
+# clean = remove all the related bins, certs, license files & nvram stored license flags
 #
 # <...> parameters are optional and appended to the basic 'speedtest' command & parameters. 
 # Type: '${BIN} -h' to see all the parameters available.
@@ -26,22 +29,23 @@
 # This script should be executed from web gui accepting EULA license agreements on the related webpage.
 #
 
-BINNAME=speedtest
-BINVER=1.0.0
-BINARCH=arm
-BINOS=linux
-BINARCNAME=ookla-${BINNAME}-${BINVER}-${BINARCH}-${BINOS}.tgz
-CERTNAME=cert.pem
+set_lics() {
+# setup license flags
+for V in ${NVARS}
+do
+eval `nvram get ${V}` >/dev/null 2>&1
+[ "`eval echo '$'${V}`" = "1" ] || nvram set ${V}=1
+done
+}
 
-BINDIR=/tmp
-CONFDIR=/tmp
-CERTDIR=/etc/ssl
-CERT=${CERTDIR}/${CERTNAME}
-BIN=${BINDIR}/${BINNAME}
+clean_files() {
+# unset temp files & license flags
+rm -rf ${BIN}* ${CONFDIR}/.config/ ${CERTDIR}/
+for V in ${NVARS}; do nvram unset ${V}; done
+}
 
-BINURL=https://bintray.com/ookla/download/download_file?file_path=${BINARCNAME}					# urls
-CERTURL=https://curl.haxx.se/ca/ca${CERTNAME}
-
+run_speedtest() {
+# download, extract files & run speedtest in non-interactive mode
 BINCMD=${BIN}													# command basic params
 [ ! ${HOME} ] && BINCMD="${BINCMD} --accept-license --accept-gdpr -p no"					# non interactive params
 
@@ -55,4 +59,39 @@ BINCMD=${BIN}													# command basic params
 export HOME=${CONFDIR}												# export non empty HOME env var
 [ ! -z "${1}" ] && BINCMD="${BINCMD} ${@}"									# append parameters if any
 nice -n -20 ${BINCMD}												# execute boosted command
+}
+
+BINNAME=speedtest
+BINVER=1.0.0
+BINARCH=arm
+BINOS=linux
+BINARCNAME=ookla-${BINNAME}-${BINVER}-${BINARCH}-${BINOS}.tgz
+CERTNAME=cert.pem
+NVARS="speedtest_net_license speedtest_net_gdpr"
+
+BINDIR=/tmp
+CONFDIR=/tmp
+CERTDIR=/etc/ssl
+CERT=${CERTDIR}/${CERTNAME}
+BIN=${BINDIR}/${BINNAME}
+
+BINURL=https://bintray.com/ookla/download/download_file?file_path=${BINARCNAME}
+CERTURL=https://curl.se/ca/ca${CERTNAME}
+
+case "${1}" in
+setlics)
+set_lics
+;;
+clean)
+clean_files
+;;
+-L|--servers)
+run_speedtest "$@"
+;;
+*)
+set_lics
+run_speedtest "$@"
+;;
+esac
+
 exit $?
