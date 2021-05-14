de25ee92ab986a11cbb05099b4f5f1f125bd4166
negan07 <negan07@users.noreply.github.com>
2021-05-15 00:24:36 +0200

    core/scripts/guiinfo.sh: removed opkg url connection ping check, other smalls

--- work/ancistrus-core-1.6.0/scripts/guiinfo.sh
+++ work/ancistrus-core-1.6.0/scripts/guiinfo.sh
@@ -14,19 +14,22 @@
 # Usage: $0 >/dev/null
 #
 
+BINDIR=/usr/sbin/
+CONFDIR=/usr/etc/
+
 DSLINFO=/tmp/ancdslinfo
 LISTUPGR=/tmp/opkg/list-upgradable
+OPKGBIN=${BINDIR}/opkg
+OPKGCONF=${CONFDIR}/opkg.conf
+ERR=0
 
 xdslctl info --version 2>${DSLINFO}					#xdslctl uses stderr for drv version show
 xdslctl info --vendor >>${DSLINFO}					#append vendor
 xdslctl info --vectoring | grep Vectoring >>${DSLINFO}			#vectoring state
 for G in Profile SNR Attn Pwr; do xdslctl info --stats | grep $G >>${DSLINFO}; done
-eval `nvram get anc_upgr_disable` >/dev/null 2>&1			#perform rep update
-	if [ "$anc_upgr_disable" != "1" -a -x /usr/sbin/opkg ]; then
-	ping -4 -c 1 -w 1 -W 1 -q `cat /usr/etc/opkg.conf | grep "src/gz " | grep -v "#" | head -n 1 | awk '{printf $3}' | cut -f 3- -d "/" | cut -f 1 -d "/"` >/dev/null 2>&1					#test if main rep is up
-		if [ $? -eq 0 ]; then					#ping is good update rep
-		opkg update >/dev/null 2>&1
-		opkg list-upgradable >${LISTUPGR}			#opkg not creating a file list-upgradable so redir the output
-		fi
+	if [ "`anc nvram drget anc_upgr_disable 0`" != "1" -a -x ${OPKGBIN} -a -r ${OPKGCONF} ]; then	#perform rep update
+	opkg update >/dev/null 2>&1
+	ERR=$?
+	opkg list-upgradable >${LISTUPGR}				#opkg not creating a file list-upgradable so redir the output
 	fi
-exit 0
+exit $ERR
